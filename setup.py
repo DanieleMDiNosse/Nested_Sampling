6a4687cb02328dbd1bcaa91a7bfa76c51f0ad8e1
Daniele Maria <danielemdinosse@gmail.com>
2021-07-20 11:03:02 +0200

    docs

---
+++ setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='Nested Sampling Implementation',
+    version='1.0.0',
+    description='A python implementation of nested sampling to compute a N-dim integral of a gaussian function',
+    url='https://github.com/DanieleMDiNosse/Nested_Sampling.git',
+    author='Di Nosse Daniele Maria',
+    author_email='danielemdinosse@gmail.com',
+    license='gnu general public license',
+    packages = find_packages(),
+    install_requires=['numpy','tqdm', 'pandas', 'matplotlib'],
+    classifiers=[
+        'Development Status :: 1 - Planning',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: POSIX :: Linux',
+        'Programming Language :: Python :: 3.8',
+    ],
+)
